5773a17145be62079b5218392f2802545126f236
asmaaabozied <49614555+asmaaabozied@users.noreply.github.com>
2025-02-25 17:21:30 +0200

    updated

---
+++ database/migrations/2022_03_23_173154_create_videos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->longText('description')->nullable();
+            $table->integer('e_provider_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('e_provider_id')->references('id')->on('e_providers')->onDelete('cascade')->onUpdate('cascade');
+       
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
